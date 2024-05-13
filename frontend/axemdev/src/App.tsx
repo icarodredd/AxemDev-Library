32be2525dd449a8da27a39adbe932149e65b63fb
icarodredd <icarog854@gmail.com>
2024-05-13 13:59:25 -0300

    feat: add logo home

--- frontend/axemdev/src/App.tsx
+++ frontend/axemdev/src/App.tsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1 className="text-darkgreenaxem">Testando..</h1>
+      <Header />
     </>
   );
 }
--- frontend/axemdev/src/components/Header.tsx
+++ frontend/axemdev/src/components/Header.tsx
@@ -1,3 +1,11 @@
 export default function Header() {
-  return {};
+  return (
+    <div className="p-8">
+      <img
+        width="100"
+        src="https://i.imgur.com/fKtVpu4.png"
+        alt="Logo da AxemDev"
+      />
+    </div>
+  );
 }
