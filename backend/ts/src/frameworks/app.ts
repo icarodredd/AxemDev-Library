f4cfc03a2b9071e39162cbd3ab63d886b6149c04
icarodredd <icarog854@gmail.com>
2024-05-23 14:49:45 -0300

    feat: add cors

--- backend/ts/src/frameworks/app.ts
+++ backend/ts/src/frameworks/app.ts
@@ -1,7 +1,16 @@
 import express from "express";
 import { router } from "../controllers/courseController.js";
+import cors from "cors";
+
 const app = express();
 
+const corsOptions = {
+  origin: "*",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use("/courses", router);
 
