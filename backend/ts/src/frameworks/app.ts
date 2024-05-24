210dbdf7db2ed68493c4cf0019fdbc83271fdf3d
icarodredd <icarog854@gmail.com>
2024-05-24 10:09:13 -0300

    fix: cors problem

--- backend/ts/src/frameworks/app.ts
+++ backend/ts/src/frameworks/app.ts
@@ -4,13 +4,12 @@ import cors from "cors";
 
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-  credentials: true,
-  optionSuccessStatus: 200,
-};
-
-app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 app.use(express.json());
 app.use("/courses", router);
 
