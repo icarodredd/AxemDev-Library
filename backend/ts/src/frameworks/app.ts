a7df157c1305283057989900b5d97a5f6dbad0ce
icarodredd <icarog854@gmail.com>
2024-05-24 10:14:48 -0300

    cors

--- backend/ts/src/frameworks/app.ts
+++ backend/ts/src/frameworks/app.ts
@@ -4,12 +4,8 @@ import cors from "cors";
 
 const app = express();
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+app.use(cors({ origin: true }));
+
 app.use(express.json());
 app.use("/courses", router);
 
